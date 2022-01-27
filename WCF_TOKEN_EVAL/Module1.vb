d67e3855f40bdf3e2bee0b7078e55f4aa43a72b0
WhiteLotus <jhonatanarroyo97@outlook.com>
2022-01-27 01:27:29 -0500

    First commit

---
+++ WCF_TOKEN_EVAL/Module1.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data.SqlClient
+Imports System.Data.Sql
+Imports System.IO
+Imports System.Runtime.Serialization
+Imports System.ServiceModel.Web
+Imports System.ServiceModel.Description
+Imports System.ServiceModel
+Imports System.ServiceModel.Activation
+
+<ServiceContract>
+Public Interface IService
+
+    <OperationContract()>
+    <WebGet(UriTemplate:="SeekToken/{DataToMerge}", ResponseFormat:=WebMessageFormat.Json, BodyStyle:=WebMessageBodyStyle.Bare)>
+    Function SeekToken(ByVal DataToMerge As String) As List(Of token_eval)
+
+End Interface
+Public Class Service
+    Implements IService
+
+    Public Function SeekToken(ByVal DataToMerge As String) As List(Of token_eval) Implements IService.SeekToken
+        Dim TokenCollection As New List(Of token_eval)()
+
+        TokenCollection.Add(New token_eval(1, DataToMerge & "Arnold29", "Daimon*/"))
+        TokenCollection.Add(New token_eval(2, "Eminnem60", "Mendez-*/"))
+        TokenCollection.Add(New token_eval(3, "Silvester889", "Stallone*@1"))
+        TokenCollection.Add(New token_eval(4, "Scarlet_J7", "johanson19998"))
+        Return TokenCollection
+    End Function
+End Class
+Module Module1
+    Sub Main()
+        Dim SetupWCF As WebServiceHost = New WebServiceHost(GetType(Service),
+                                         New Uri("http://localhost:8000/"))
+        SetupWCF.Open()
+        Console.WriteLine("Press <ENTER> to stop the WCF Self-Hosted")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ WCF_TOKEN_EVAL/token_eval.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Runtime.Serialization
+<DataContract>
+Public Class token_eval
+
+    Private m_IdToken As Integer
+    Private m_Username As String
+    Private m_Password As String
+    <DataMember>
+    Public Property IdToken() As Integer
+        Get
+            Return m_IdToken
+        End Get
+        Set(ByVal value As Integer)
+            m_IdToken = value
+        End Set
+    End Property
+    <DataMember>
+    Public Property Username() As String
+        Get
+            Return m_Username
+        End Get
+        Set(ByVal value As String)
+            m_Username = value
+        End Set
+    End Property
+    <DataMember>
+    Public Property Password() As String
+        Get
+            Return m_Password
+        End Get
+        Set(ByVal value As String)
+            m_Password = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal IdToken As Integer, ByVal Username As String, ByVal Password As String)
+        Me.m_IdToken = IdToken
+        Me.m_Username = Username
+        Me.m_Password = Password
+    End Sub
+End Class
